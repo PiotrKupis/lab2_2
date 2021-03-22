b0f74f3907e7177f08f0d88439417e26a495aaa6
Piotr.Kupis <222488@edu.p.lodz.pl>
2021-03-22 18:17:36 +0100

    Added test that calculates similarity of sequences of the different numbers

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -11,12 +11,22 @@ class SimilarityFinderTest {
     private SimilarityFinder similarityFinder;
     private int[] firstArray = new int[]{1};
     private int[] copyOfFirstArray = new int[]{1};
+    private int[] secondArray = new int[]{2};
 
     SequenceSearcher searcherThatAlwaysReturnsTrue = new SequenceSearcher() {
         @Override
         public SearchResult search(int elem, int[] sequence) {
             SearchResult.Builder builder = SearchResult.builder();
             builder.withFound(true);
+            return builder.build();
+        }
+    };
+
+    SequenceSearcher searcherThatAlwaysReturnsFalse = new SequenceSearcher() {
+        @Override
+        public SearchResult search(int elem, int[] sequence) {
+            SearchResult.Builder builder = SearchResult.builder();
+            builder.withFound(false);
             return builder.build();
         }
     };
@@ -31,5 +41,13 @@ class SimilarityFinderTest {
         assertEquals(expectedValue, result);
     }
 
+    @Test
+    void shouldReturnZero() {
+        double result, expectedValue = 0;
 
+        similarityFinder = new SimilarityFinder(searcherThatAlwaysReturnsFalse);
+        result = similarityFinder.calculateJackardSimilarity(firstArray, copyOfFirstArray);
+
+        assertEquals(expectedValue, result);
+    }
 }
