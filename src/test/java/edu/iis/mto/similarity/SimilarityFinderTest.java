73bfa7da045a2366712cc69e03b08baa8b608494
Piotr.Kupis <222488@edu.p.lodz.pl>
2021-03-22 18:15:31 +0100

    Added test that calculates similarity of sequences of the same numbers

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,35 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SimilarityFinderTest {
 
+    private SimilarityFinder similarityFinder;
+    private int[] firstArray = new int[]{1};
+    private int[] copyOfFirstArray = new int[]{1};
+
+    SequenceSearcher searcherThatAlwaysReturnsTrue = new SequenceSearcher() {
+        @Override
+        public SearchResult search(int elem, int[] sequence) {
+            SearchResult.Builder builder = SearchResult.builder();
+            builder.withFound(true);
+            return builder.build();
+        }
+    };
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void shouldReturnOne() {
+        double result, expectedValue = 1;
+
+        similarityFinder = new SimilarityFinder(searcherThatAlwaysReturnsTrue);
+        result = similarityFinder.calculateJackardSimilarity(firstArray, copyOfFirstArray);
+
+        assertEquals(expectedValue, result);
     }
+
 
 }
